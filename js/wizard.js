7653c5b19b4f3c4611d8867f0f95bf426456add4
Omar Einea <eineao@gmail.com>
2015-12-13 11:16:54 +0400

    Clean up and reorganize stuff

--- js/wizard.js
+++ js/wizard.js
@@ -1,35 +1,22 @@
 (function(namespace, $) {
 	"use strict";
 
-	var DemoFormWizard = function() {
+	var Wizard = function() {
 		// Create reference to this instance
-		var o = this;
+		var w = this;
 		// Initialize app when document is ready
 		$(document).ready(function() {
-			o.initialize();
+			w.initWizard();
 		});
 
 	};
-	var p = DemoFormWizard.prototype;
+	var p = Wizard.prototype;
 
-	// =========================================================================
-	// INIT
-	// =========================================================================
-
-	p.initialize = function() {
-		this._initWizard1();
-		this._initWizard2();
-	};
-
-	// =========================================================================
-	// WIZARD 1
-	// =========================================================================
-
-	p._initWizard1 = function() {
+	p.initWizard = function() {
 		var o = this;
-		$('#rootwizard1').bootstrapWizard({
+		$('#wizard').bootstrapWizard({
 			onTabShow: function(tab, navigation, index) {
-				o._handleTabShow(tab, navigation, index, $('#rootwizard1'));
+				o._handleTabShow(tab, navigation, index, $('#wizard'));
 			}
 		});
 	};
@@ -47,40 +34,5 @@
 		$('.form-wizard-horizontal').find('.progress').css({'width': percentWidth});
 	};
 	
-	// =========================================================================
-	// WIZARD 1
-	// =========================================================================
-
-	p._initWizard2 = function() {
-		var o = this;
-		$('#rootwizard2').bootstrapWizard({
-			onTabShow: function(tab, navigation, index) {
-				o._handleTabShow(tab, navigation, index, $('#rootwizard2'));
-			},
-	  		onNext: function(tab, navigation, index) {
-				var form = $('#rootwizard2').find('.form-validation');
-	  			var valid = form.valid();
-	  			if(!valid) {
-	  				form.data('validator').focusInvalid();
-	  				return false;
-	  			}
-	  		}
-		});
-	};
-
-	p._handleTabShow = function(tab, navigation, index, wizard){
-		var total = navigation.find('li').length;
-		var current = index + 0;
-		var percent = (current / (total - 1)) * 100;
-		var percentWidth = 100 - (100 / total) + '%';
-		
-		navigation.find('li').removeClass('done');
-		navigation.find('li.active').prevAll().addClass('done');
-		
-		wizard.find('.progress-bar').css({width: percent + '%'});
-		$('.form-wizard-horizontal').find('.progress').css({'width': percentWidth});
-	};
-	
-	// =========================================================================
-	namespace.DemoFormWizard = new DemoFormWizard;
+	namespace.Wizard = new Wizard;
 }(this.materialadmin, jQuery)); // pass in (namespace, jQuery):
